c0c5406db4a79eba1458bf205ed75321d83e2f65
Diestadotcom <nickdiesta1@gmail.com>
2026-04-11 17:53:27 +0800

    Refactor Program.cs for StudentRegistration app
    Updated namespace and changed the form to start for student registration.

--- Program.cs
+++ Program.cs
@@ -1,22 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System;
 using System.Windows.Forms;
 
-namespace StudentRegistrationApp
+namespace StudentRegistration
 {
-    internal static class Program
+    static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
+        [STAThread] // <--- This is the attribute
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            // This starts your specific form
+            Application.Run(new StudentRegistration());
         }
     }
 }
